4d640528a588c3c66c1806aa53c58da1dde5dff8
saitTalhaNisanci <s.talhanisanci@gmail.com>
2018-04-04 22:39:53 +0300

    add test structure

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require("./Migrations.sol");
-
+var lottery = artifacts.require("./lottery.sol");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(lottery);
 };
--- test/lottery.js
+++ test/lottery.js
@@ -1,11 +1,18 @@
 var lottery = artifacts.require("./lottery.sol");
+var keccak256 = require('js-sha3').keccak256;
+
 
 contract('lottery', function(accounts) {
-  it("should put 0 coin in the first account", function() {
+  it("should buy a ticket", function() {
     return lottery.deployed().then(function(instance) {
-      return instance.getBalance.call(accounts[0]);
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 0, "0 wasn't in the first account");
+      console.log(accounts);
+      hash  =  keccak256(accounts[0]);
+      return instance.buyFullTicket.sendTransaction([hash,hash,hash]);
+    }).then(function(result ) {
+        console.log(result);
+    }).catch(function(e){
+      console.log(e);
     });
+
   });
 });
